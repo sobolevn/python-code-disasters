cc52181c8c3c48fa3f20185e3ffa8326bec7a1b2
sobolevn <mail@sobolevn.me>
2015-08-22 00:20:58 +0300

    readme

---
+++ README.md
@@ -0,0 +1,18 @@
+# python-code-disasters
+
+## What is it all about?
+I am, due to my work, seeing a lot of code written by other developers. Sometimes this code is so bad, that it is worth showing to the outer world.
+
+## [Privacy](#privacy)
+Privacy is very important. There are two things basically: 
+
+1. Refactor your code to remove anything, that might violate any security requirements, corporate rules or license agreements.
+2. It is not a goal of this project to insult or offend anyone, so, please, remove any brand-names, user marks, `__author__` variables and so on.
+
+## Contributing
+Feel free to contribute. Before contributing, please, double check the [Privacy](#privacy) section.
+Refactor your code to remove as much as you can to leave just the most valuable parts. I think that submitting a broken code is not an issue for this project. Moreover, formatting the code is also not required. Sometimes it is even better to leave it's formation untouched.
+
+It is generally a good practice to read through your old files and contribute your own code.
+
+It is still not clear to me, how to structure this project. 
--- django/views.py
+++ django/views.py
@@ -5,3 +5,30 @@ def create_waybill(request, id):
         return create_response([id], user_friendly=True)
     else:
         return HttpResponseRedirect("/")
+
+
+def map_reduce_task(reuqest, ids):
+    regusters = get_registers(request)
+    ids = get_ids(ids)
+    if not registers:
+        return HttpResponseRedirect("/")
+    else:
+        for register in registers:
+            if object_ids:  # Using optimized queries:
+                objects = register.objects.filter(id__in=object_ids).values_list("id", flat=True)
+            else:
+                objects = register.objects.all().values_list("id", flat=True)
+
+            t = 0
+            task_map = []
+
+            def chunks(objects, length):  # Defining method in a loop.
+                for i in xrange(0, len(objects), length):
+                    yield parcels[i:i+length]
+
+            for chunk in chunks(objects, 20):
+                countdown = 5*t
+                t += 1
+                tasks_map.append(request_by_mapper(register, chunk, countdown, datetime.now()))
+        g = group(*tasks_map)
+        reduce_task = chain(g, create_request_by_reduce_async.s(tasks_map))()
