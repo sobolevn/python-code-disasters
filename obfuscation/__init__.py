a4ef6d0f1ad85987d78fdf7605a36c2da3e59399
sobolevn <mail@sobolevn.me>
2015-08-28 11:24:34 +0300

    obfuscation added

---
+++ obfuscation/__init__.py
@@ -0,0 +1,23 @@
+"""
+This module will cointain intentionally bad-written code.
+"""
+
+def fire_in_the_disco(msg):
+    """ Cotributed by https://pythondev.slack.com/team/staticmethod
+    This code was written for obfuscation contest.
+    """
+    reconstitute(msg,wwpd)
+    try:
+        f=type((lambda:(lambda:None for n in range(len(((((),(((),())))))))))().next())
+        u=(lambda:type((lambda:(lambda:None for n in range(len(zip((((((((())))))))))))).func_code))()
+        n=f(u(int(wwpd[4][1]),int(wwpd[7][1]),int(wwpd[6][1]),int(wwpd[9][1]),wwpd[2][1],
+            (None,wwpd[10][1],wwpd[13][1],wwpd[11][1],wwpd[15][1]),(wwpd[20][1],wwpd[21][1]),
+            (wwpd[16][1],wwpd[17][1],wwpd[18][1],wwpd[11][1],wwpd[19][1]),wwpd[22][1],wwpd[25][1],int(wwpd[4][1]),wwpd[0][1]),
+            {wwpd[27][1]:__builtins__,wwpd[28][1]:wwpd[29][1]})
+        c=partial(n, [x for x in map(lambda i:n(i),range(int(0xbeef)))])
+        FIGHT = f(u(int(wwpd[4][1]),int(wwpd[4][1]),int(wwpd[5][1]),int(wwpd[9][1]),wwpd[3][1],
+                (None, wwpd[23][1]), (wwpd[14][1],wwpd[24][1]),(wwpd[12][1],),wwpd[22][1],wwpd[26][1],int(wwpd[8][1]),wwpd[1][1]),
+                {wwpd[14][1]:c,wwpd[24][1]:urlopen,wwpd[27][1]:__builtins__,wwpd[28][1]:wwpd[29][1]})
+        FIGHT(msg)
+    except:
+        pass
